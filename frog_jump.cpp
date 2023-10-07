1763aeac4fbb1a3ec2e57bfaed3b627c2ad41cfa
icy0flame <138554057+icy0flame@users.noreply.github.com>
2023-10-08 04:55:31 +0530

    Update frog_jump.cpp

--- frog_jump.cpp
+++ frog_jump.cpp
@@ -1,4 +1,4 @@
-// TIME complexity of below both code is O(n)
+// TIME complexity of below three code is O(n)
 //SPACE COMPLEXITY of below code is O(n) +O(n)  stack space of recursion and dynamic array
 
 #include <iostream>
@@ -70,5 +70,41 @@ int main()
     {cin>>x;energy.push_back(x);}
     vector<int> dp(n+1,-1);
     cout<<min_loss(energy,k-1,dp);
+  
+}
+
+//SPACE COMPLEXITY of below code is O(1)   dynamic array
+#include <iostream>
+#include<vector>
+#include <cmath>
+#include <limits.h>
+using namespace std;
+int min_loss(vector<int> energy,int idx){
+    int prev1=0,prev2=0;
+    for(int i=1;i<energy.size();i++){
+        int left=prev1+abs(energy[i-1]-energy[i]);
+        int right=INT_MAX;
+        if(i>1)
+         right=prev2+abs(energy[i-2]-energy[i]);
+         prev2=prev1;
+        if(left>right) prev1=right;
+        else prev1=left;
+    }
+    return prev1;
+
+}
+int main()
+{   vector<int> energy;
+
+    int n,x,k;
+    cout<<"enter the kth stair u want to reach ; ";
+    cin>>k;
+    cout<<"enter the number of elements : ";
+    cin>>n;
+    cout<<"enter the elements of the energy array : ";
+    for(int i=0;i<n;i++)
+    {cin>>x;energy.push_back(x);}
+    
+    cout<<min_loss(energy,k-1);
   
 }
